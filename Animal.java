3e99e7ef70166d4444aa9d1d4e88ef6588df8efc
Arnav100 <parashararnav2@gmail.com>
2019-05-03 13:51:23 -0400

    saving

--- Animal.java
+++ Animal.java
@@ -7,9 +7,40 @@
  */
 public abstract class Animal
 {
-    public int sampleMethod(int y)
+    private int age;
+    private int maxAge;
+    private double mass;
+    private double maxMass;
+    private boolean isAlive;
+   
+    public Animal(double startingMass, double maxMass, int maxAge)
     {
+        this.mass = startingMass;
+        this.maxMass = maxMass;
+        this.maxAge = maxAge;
+        this.isAlive = true;
+    }
     
-        return y;
+    
+    public void birthday()
+    {
+        age++;
+    }
+    
+    public int getAge(int age)
+    {
+        return age;
+    }
+    
+    public void eat(double mass)
+    {
+        this.mass += mass;
+        if(mass > maxMass)
+            mass = maxMass;
+    }
+    
+    public void die()
+    {
+        isAlive = false;
     }
 }
