aeffeddbba6a8da39013194b91513e8190047ba8
dananigrin <dana.nigrin@gmail.com>
2019-05-03 13:48:12 -0400

    Added new files

---
+++ Goat.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Goat here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Goat extends Animal implements Herbivore
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Goat
+     */
+    public Goat()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
